753fa6f4bdf9cb7b08d516a3ecc603ae3fd70bb6
Danny Tseng <github email address>
2019-05-22 15:00:26 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, presence: true, exclusion: {:in => ['too short']}
+  validates :category, exclusion: {:in => ["Speculative Fiction"]}
 end
